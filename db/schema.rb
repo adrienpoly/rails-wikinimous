6bb827b784930fe1b8ef4e1a00d9fc023c13f82d
Adrien Poly <adrienpoly@gmail.com>
2017-03-14 09:40:03 +0100

    changed db to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170214154804) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-10.times do |iteration|
+100.times do |iteration|
   article = Article.new
   article.title = Faker::Lorem.sentence
-  article.content = Faker::Lorem.paragraph(5)
+  article.content = Faker::Lorem.paragraph(50)
   article.save
 
 end
